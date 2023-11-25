b63ce2955f68fa1c8b4b9874928eb515c0ea03bd
ilham kusmayadi <lifehelper1733@gmail.com>
2023-11-26 00:12:25 +0700

    menambahkan service

--- asset/index.js
+++ asset/index.js
@@ -1,31 +1,27 @@
 $(document).ready(function () {
-  // Fungsi untuk mengaktifkan/menonaktifkan Dark Mode
   $("#darkModeToggle").click(function () {
     $("body").toggleClass("dark-mode");
   });
 });
 
 // togler
- const navbarTogglerBtn = document.getElementById("navbarTogglerBtn");
- const navbarCollapse = document.getElementById("navbarNav");
+const navbarTogglerBtn = document.getElementById("navbarTogglerBtn");
+const navbarCollapse = document.getElementById("navbarNav");
 
- // Add event listener to the toggler button
- navbarTogglerBtn.addEventListener("click", () => {
-   // Toggle the 'show' class on navbar-collapse
-   navbarCollapse.classList.toggle("show");
- });
+navbarTogglerBtn.addEventListener("click", () => {
+  navbarCollapse.classList.toggle("show");
+});
 
 //  download CV
 
- function downloadCV() {
-   var cvPath = "cv/cv_ilham.pdf";
+function downloadCV() {
+  var cvPath = "cv/cv_ilham.pdf";
 
-   var link = document.createElement("a");
-   link.href = cvPath;
-   link.download = "Ilham_Kusmayadi_CV.pdf";
-   link.target = "_blank";
-   document.body.appendChild(link);
-   link.click();
-   document.body.removeChild(link);
- }
-
+  var link = document.createElement("a");
+  link.href = cvPath;
+  link.download = "Ilham_Kusmayadi_CV.pdf";
+  link.target = "_blank";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
